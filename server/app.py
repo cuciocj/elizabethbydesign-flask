683e772e4f325c295f0a48a0c6033295af7c9a03
CJ Cucio <cuciocj@gmail.com>
2020-07-07 11:10:23 +1200

    added methods

--- server/app.py
+++ server/app.py
@@ -11,8 +11,40 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
-# test endpoint
-@app.route('/hello', methods=['GET'])
+# TODO: get data from db
+@app.route('/footer', methods=['GET'])
+def get_footer():
+
+    response_object = {
+        'info': {
+            'mobile': '(64)22 123 4567',
+            'email': 'ebdesign@gmail.com',
+            'address': 'Auckland, New Zealand'
+        },
+        'links': {
+            'facebook': 'https://www.facebook.com/cuciocj',
+            'instagram': 'https://www.instagram.com/cuciocj',
+            'twitter': 'https://www.twitter.com/cuciocj'
+        }
+    }
+    
+    return jsonify(response_object)
+
+@app.route('/home', methods=['GET'])
+def get_home():
+
+    response_object = {
+        'welcome_jumbotron': {
+            'header': 'Elizabeth by Design',
+            'subheader': 'Welcome to Elizabeth by Design,' 
+                + ' where we work with you to create your perfect outfit,' 
+                + ' whether it be for casual, formal or work.'
+        }
+    }
+
+    return jsonify(response_object)
+
+@app.route('/test', methods=['GET'])
 def hello():
     return jsonify('hello world!')
 
